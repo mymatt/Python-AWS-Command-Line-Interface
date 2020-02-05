ab9e0fe06278450ecf90f5668feddc911a7dfc3b
Matthew Myers <mym3491@yahoo.com.au>
2020-02-05 13:58:50 +1100

    added function create bucket

--- s3.py
+++ s3.py
@@ -18,3 +18,10 @@ def cli(profile):
     session = boto3.Session(profile_name=profile)
     # s3 = session.resource('s3')
     bucket_admin = BucketAdmin(session)
+
+
+@cli.command('create-bucket')
+@click.argument('bucket')
+def create_bucket(bucket):
+    """Create bucket."""
+    bucket_admin.setup_bucket(bucket)
