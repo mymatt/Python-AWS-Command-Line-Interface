c4c7560fa5c865ddb4511b1967cf9995fb8599ac
Matthew Myers <mym3491@yahoo.com.au>
2020-02-05 13:56:35 +1100

    added new bucket file class

--- s3.py
+++ s3.py
@@ -3,7 +3,18 @@
 import boto3
 import click
 
-from s3.bucket import BucketAdmin
+from bucket import BucketAdmin
 
 session = None
 bucket_admin = None
+
+@click.group()
+@click.option('--profile', default='s3play', help='AWS Profile')
+def cli(profile):
+
+    global session, bucket_admin
+
+    # if profile not set, AWS looks for environmental variables
+    session = boto3.Session(profile_name=profile)
+    # s3 = session.resource('s3')
+    bucket_admin = BucketAdmin(session)
